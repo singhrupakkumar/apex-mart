2777e4946b08d8654740ed0f2effbdc7e60c75d6
Rupak Singh <rupak@einaim.org>
2021-06-18 13:47:49 +0530

    first commit

---
+++ components/shared/footers/modules/FooterCopyright.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const FooterCopyright = () => (
+    <div className="ps-footer__copyright">
+        <p>&copy;  2021 Apex Mart. All Rights Reserved</p>
+        <p>
+            <span>We Using Safe Payment For:</span>
+            <a href="#">
+                <img src="/static/img/payment-method/1.jpg" alt="Apex Mart" />
+            </a>
+            <a href="#">
+                <img src="/static/img/payment-method/2.jpg" alt="Apex Mart" />
+            </a>
+            <a href="#">
+                <img src="/static/img/payment-method/3.jpg" alt="Apex Mart" />
+            </a>
+            <a href="#">
+                <img src="/static/img/payment-method/4.jpg" alt="Apex Mart" />
+            </a>
+            <a href="#">
+                <img src="/static/img/payment-method/5.jpg" alt="Apex Mart" />
+            </a>
+        </p>
+    </div>
+);
+
+export default FooterCopyright;
