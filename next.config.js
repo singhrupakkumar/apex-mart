ffaecfed7da602d9923f2211153bdf48548ccde9
Rupak Singh <rupak@einaim.org>
2021-06-21 16:13:33 +0530

    ud5

--- next.config.js
+++ next.config.js
@@ -8,15 +8,27 @@
 * */
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const nextSettings = {
     env: {
         title: 'Apex Mart',
         titleDescription: 'Home',
     },
     optimization: {
-      minimize: true,  
-   },
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  },
 };
 
 module.exports = withPlugins([withImages(), nextSettings]);
